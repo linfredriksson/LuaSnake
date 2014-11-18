68168fa5ba3e6965124a7033884339d2e36dc886
Linus Fredriksson <linfredriksson@gmail.com>
2014-11-18 22:52:08 +0100

    - Added random seed - Store snake and apples in grid, then when rendering sizes are defined, this was done to separate rendering from storage making it easier to change the visual part - More comments and other

--- main.lua
+++ main.lua
@@ -6,13 +6,18 @@ function love.load()
   backgroundColor = {255, 255, 255}
   love.graphics.setBackgroundColor(backgroundColor)
   
+  -- set random seed to prevent apples from always
+  -- starting at the same positions
+  math.randomseed(os.time())
+  
   -- global variables
+  gameIsRunning = false
   timer = 0
   enterText = false
   inputText = ""
   defaultLength = 5
-  worldHeight = 26
-  worldWidth = 56
+  worldHeight = 28
+  worldWidth = 58
   
   -- setup highscore table
   highscore = {}
@@ -21,7 +26,7 @@ function love.load()
   -- initiate snakes
   snake = {
     {color = {255, 0, 0}, body = {}, start = {x = 20, y = 13}, keys = {"w", "s", "a", "d"}, score = 0, direction = {x = 1, y = 0}},
-    {color = {0, 255, 0}, body = {}, start = {x = 46, y = 13}, keys = {"i", "k", "j", "l"}, score = 0, direction = {x = -1, y = 0}}
+    {color = {0, 255, 0}, body = {}, start = {x = 38, y = 13}, keys = {"i", "k", "j", "l"}, score = 0, direction = {x = -1, y = 0}}
   }
   for i = 1, #snake do setupSnake(snake[i], defaultLength) end
 
@@ -63,8 +68,8 @@ function generateApples(n)
     while i < 20 do
       i = i + 1
       pos = {
-        x = 20 + math.random(56) * 10,
-        y = 120 + math.random(26) * 10
+        x = math.random(worldWidth),
+        y = math.random(worldHeight)
       }
       
       for j = 1, #snake do
@@ -86,14 +91,17 @@ function generateApples(n)
 end
 
 --[[
+  Helper function used to compare two positions
 ]]--
 function equal(a, b) return a.x == b.x and a.y == b.y end
 
 --[[
+  Helper function used when sorting the high score table
 ]]--
 function compare(a, b) return a.score > b.score end
 
 --[[
+  Initiate a snake by creating its body
 ]]--
 function setupSnake(snake, l)
   snake.body = {}
@@ -101,12 +109,13 @@ function setupSnake(snake, l)
   
   for i = 1, l do
     table.insert(snake.body, {
-      x = snake.start.x - i * snake.direction.x * 10,
-      y = snake.start.y - i * snake.direction.y * 10})
+      x = snake.start.x - i * snake.direction.x,
+      y = snake.start.y - i * snake.direction.y})
   end
 end
 
 --[[
+  Save highs core to file
 ]]--
 function saveHighscore()
   -- insert the new highscore and sort the list
@@ -118,6 +127,7 @@ function saveHighscore()
 end
 
 --[[
+  Load highs core table from file
 ]]--
 function loadHighscore()
   --if love.filesystem.exists("highscorea.lua") then -- check if this actually works
@@ -126,21 +136,27 @@ function loadHighscore()
 end
 
 --[[
+  Reads user input
 ]]--
 function love.textinput(t)
   if enterText then inputText = inputText .. t end
 end
 
 --[[
+  Handles pressed keys
 ]]--
 function love.keypressed(key)
   if key == "escape" then love.event.quit() end
-  if key == "return" then enterText =  not enterText end
   if key == "backspace" then inputText = string.sub(inputText, 1, -2) end
+  if key == "return" then
+    gameIsRunning = not gameIsRunning
+    timer = 0
+  end
   
+  if key == "t" then enterText =  not enterText end
   if not enterText and key == "g" then saveHighscore() end
   
-  if not enterText then
+  if not enterText and gameIsRunning then
     for i = 1, #snake do
       if key == snake[i].keys[1] and snake[i].direction.y ~= 1 then snake[i].direction = {x = 0, y = -1} end
       if key == snake[i].keys[2] and snake[i].direction.y ~= -1 then snake[i].direction = {x = 0, y = 1} end
@@ -151,17 +167,18 @@ function love.keypressed(key)
 end
 
 --[[
+  Main game loop, updates the game progression
 ]]--
 function love.update(dt)
   timer = timer + dt
-  if not enterText then
+  if not enterText and gameIsRunning then
     if timer > 0.1 then
       timer = timer - 0.1
       
       for i = 1, #snake do
         local pos = {x = snake[i].body[1].x, y = snake[i].body[1].y}
-        pos.x = pos.x + snake[i].direction.x * 10
-        pos.y = pos.y + snake[i].direction.y * 10
+        pos.x = pos.x + snake[i].direction.x
+        pos.y = pos.y + snake[i].direction.y
         
         local hit = false
         for j = 1, #apples do
@@ -174,28 +191,28 @@ function love.update(dt)
           end
         end
         
-        if not hit then
+        --if not hit then
           table.remove(snake[i].body)
           table.insert(snake[i].body, 1, pos)
-        end
+        --end
         
-        if pos.x <= 10 then snake[i].direction = {x = 1, y = 0} end
-        if pos.x >= love.graphics:getWidth() - 20 then snake[i].direction = {x = -1, y = 0} end
-        if pos.y <= 120 then snake[i].direction = {x = 0, y = 1} end
-        if pos.y >= love.graphics:getHeight() - 20 then snake[i].direction = {x = 0, y = -1} end
-        --setupSnake
+        if pos.x <= 1 then snake[i].direction = {x = 1, y = 0} end
+        if pos.x >= worldWidth then snake[i].direction = {x = -1, y = 0} end
+        if pos.y <= 1 then snake[i].direction = {x = 0, y = 1} end
+        if pos.y >= worldHeight then snake[i].direction = {x = 0, y = -1} end
       end
     end
   end
 end
 
 --[[
+  Draw function
 ]]--
 function love.draw()
   -- draw border
   love.graphics.setColor(wallColor)
-  love.graphics.rectangle("fill", 10, 10, love.graphics:getWidth() - 20, 100)
-  love.graphics.rectangle("fill", 10, 120, love.graphics:getWidth() - 20, love.graphics:getHeight() - 130)
+  love.graphics.rectangle("fill", 10, 10, love.graphics:getWidth() - 20, 90)
+  love.graphics.rectangle("fill", 10, 110, love.graphics:getWidth() - 20, love.graphics:getHeight() - 120)
   
   love.graphics.setColor(backgroundColor)
   
@@ -212,17 +229,21 @@ function love.draw()
   for i = 1, #snake do
     love.graphics.setColor(snake[i].color)
     
-    love.graphics.print("PLAYER:" .. i, 20 + 60 * (i - 1), 20)
-    love.graphics.print("SCORE:" .. #snake[i].body, 20 + 60 * (i - 1), 40)
+    love.graphics.print("PLAYER:" .. i, 20 + 70 * (i - 1), 20)
+    love.graphics.print("SCORE:" .. #snake[i].body, 20 + 70 * (i - 1), 40)
     
     for j = 1, #snake[i].body do
-      love.graphics.rectangle("fill", snake[i].body[j].x, snake[i].body[j].y, 10, 10)
+      love.graphics.rectangle(
+        "fill",
+        snake[i].body[j].x * 10,
+        100 + snake[i].body[j].y * 10,
+        10, 10)
     end
   end
   
   -- render apples
   for i = 1, #apples do
     love.graphics.setColor(200, 55, 55)
-    love.graphics.rectangle("fill", apples[i].x, apples[i].y, 10, 10)
+    love.graphics.rectangle("fill", apples[i].x * 10, 100 + apples[i].y * 10, 10, 10)
   end
 end
